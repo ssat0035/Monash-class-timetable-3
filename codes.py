5a02c54265521d9911dac005a8657f5a2792413a
ssat0035 <ssat0035@student.monash.edu>
2024-10-14 20:19:59 +1100

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://classtimetable.monash.edu/2025/")
+
+element = driver.find_element(By.ID, "element_id")
+
