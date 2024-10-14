9dbf8460c3993550a7e30d914d943190d9e34bca
ssat0035 <77872087+shu279@users.noreply.github.com>
2024-10-14 20:35:06 +1100

    Update codes.py

--- codes.py
+++ codes.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://classtimetable.monash.edu/2025/")
 
+
 element = driver.find_element(By.ID, "element_id")
 
 
